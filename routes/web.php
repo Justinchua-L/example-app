7361d5feef26d65cdcd8f87bdcdc87ab1137376e
Justinchua-L <justin.chua@benilde.edu.ph>
2022-07-16 23:10:19 +0800

    practice 1 , route and views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// continue study : template and views videos no.2
+
+Route::get('/homeindex', function () {
+    return view('home.index',[]);
+})->name('home.index');
+
+Route::get('/posts/{id}', function ($id) {
+
+   return 'Blog post' . $id;
+ })
+//  ->where([   # use where to add constrain   // gloablly check RouteServiceProvider.php line 39
+//  'id' => '[0-9]+' #'+' sign means need atleast 1 character
+//  ])
+ ->name('posts.show');
+ 
+
+
+// '?' use for optional parameter 
+Route::get('/recent-post/{days_ago?}', function ($daysAgo=20) {
+
+   return 'Posts from  ' . $daysAgo . 'days ago';
+})->name('posts.recent.index');
+
